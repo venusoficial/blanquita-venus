41d74e9723d3d3cc285ddef4aaa81e1a431850de
blanquita-venus <seba9727@gmail.com>
2026-07-10 02:32:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+body {
+    margin: 0;
+    background: #000;
+    color: white;
+    font-family: Arial, Helvetica, sans-serif;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    text-align: center;
+}
 
+h1 {
+    color: #ff4fa3;
+}
+
+p {
+    color: #ffffff;
+}
